0dc0cf0d26dd1318d10f3d4e5e9380a12f44a84f
Ivanna0407 <129338460+Ivanna0407@users.noreply.github.com>
2023-09-14 09:26:52 -0600

    Create Funciones.py

---
+++ Funciones.py
@@ -0,0 +1,20 @@
+def f(x):
+  f_x=-abs(3*x)
+  return f_x
+def y(x):
+  y_x=-2*x+34
+  return y_x
+def z(x):
+  z_x=(-x/3)-10
+  return z_x
+def w(x):
+  w_x=(x-2)**2
+  return w_x
+q=f(5)
+print(q)
+q=y(5)
+print(q)
+q=z(5)
+print(q)
+q=w(5)
+print(q)
