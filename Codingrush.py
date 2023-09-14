60712b83fb1de2c779638e631dc830a05dd90f52
Ivanna0407 <129338460+Ivanna0407@users.noreply.github.com>
2023-09-14 09:33:06 -0600

    Create Codingrush.py

---
+++ Codingrush.py
@@ -0,0 +1,7 @@
+print("¿Cuál es la temperatura en grados Celcius?")
+c=input()
+c=int(c)
+f=((9*c)/5)+32
+k=c+273.15
+print("La temperatura en grados Farenheit es:",f)
+print("Y en grados Kelvin es:",k)
