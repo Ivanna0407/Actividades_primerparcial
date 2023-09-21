e5ecd322091a4f68fe81501944625ea977b59a5f
Ivanna0407 <129338460+Ivanna0407@users.noreply.github.com>
2023-09-21 09:15:18 -0600

    Create codolimpiadas_3.py

---
+++ codolimpiadas_3.py
@@ -0,0 +1,12 @@
+def multipicacion (a,b):
+  x=a*b
+  return x
+def división (a,b):
+  x=a//b
+  return x
+
+print("Dame el primer número:")
+a=int(input())
+print("Dame el segundo número;")
+b=int(input())
+print("la multiplicación es:",multipicacion(a,b),"La división es ",división(a,b))
